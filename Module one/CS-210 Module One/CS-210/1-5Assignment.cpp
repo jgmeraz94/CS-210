8f2a8d547a245166b1331cc0f84efdf31dfeb675
Juan Gonzalez <jgmeraz94@gmail.com>
2025-06-03 01:39:24 -0500

    Moduel one Assignment added

---
+++ Module one/CS-210 Module One/CS-210/1-5Assignment.cpp
@@ -0,0 +1,14 @@
+// Juan Gonzalez, May 9th, 2025
+// This is a simple program to output the message "Hello World!" to the console
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main() {
+    string helloWorld = "Hello World!"; //Using a string variable to store the message
+
+    cout << helloWorld << endl; //Output the message to the console
+
+    return 0; // Returns 0 to indicate there was no errors
+}
